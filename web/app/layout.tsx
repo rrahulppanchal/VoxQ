65f15afea28592d346b0b9b3ad63264efb2550c3
Rahul Panchal <rahulpanchaloff@hotmail.com>
2023-12-02 01:11:01 +0530

    ui changes

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,10 @@ export const fontSans = FontSans({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      {children}
+      <Toaster />
+      </body>
     </html>
   );
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,39 @@
+"use client"
 import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
 import Image from 'next/image';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default function Home() {
+  const { toast } = useToast()
   return (
     <main className="">
-      <Button>Click me</Button>
+      <Button onClick={()=>{
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+          action: <h4>Try again</h4>,
+        })
+      }}>Click me</Button>
+      <Select onValueChange={(e : React.ChangeEvent<HTMLSelectElement>)=>{
+        console.log(e)
+      }} >
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>
     </main>
   );
 }
